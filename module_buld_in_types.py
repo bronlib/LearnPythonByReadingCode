2cba0e127463ea826ca7382d5574bed716340cad
bronlib <robert@brondijk.com>
2023-11-28 19:13:54 +0100

    Updated : the line seems to be ok now

--- module_buld_in_types.py
+++ module_buld_in_types.py
@@ -1,22 +1,23 @@
 """
 Build in Type:
    reference: https://docs.python.org/3/library/stdtypes.html
+              https://docs.python.org/3/reference/datamodel.html#objects-values-and-types
 """
 
 print("******** Start of module ", __name__, "********")
 
-
 """
-The principal built-in types are 
-     numerics, 
-     sequences, 
+The principle  built-in types are 
+     None
+     numerics : bool,  int, float, complex
+     sequences: str, bytes
      mappings, 
      classes, 
      instances
      exceptions
 
-
 """
+
 
 
 
